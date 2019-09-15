3100451f8bc3ce78e0ea2a56c230bf01ed1300ae
Faizuddin Mohammed <mdfaizuddin15@gmail.com>
2019-09-15 17:02:22 +0530

    Replace gulp with parcel

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const htmlmin = require("gulp-htmlmin");
-
-gulp.task("minify", () => {
-  return gulp
-    .src("*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist"));
-});
